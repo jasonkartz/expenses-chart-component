9d68f86d96815d93ff57ec7ae6a639f10caeaad7
Jason Kartz <jasonkartz@gmail.com>
2022-06-24 19:57:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Attribution from "./Attribution";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun
+        Total this month $478.33 +2.4% from last month
+      </p>
+      <Attribution />
+    </>
   );
 }
 
