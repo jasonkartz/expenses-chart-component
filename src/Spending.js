1fd830d944e26635937ffb7b3e495dc2163ec3ea
Jason Kartz <jasonkartz@gmail.com>
2022-06-26 23:57:24 -0500

    update

--- src/Spending.js
+++ src/Spending.js
@@ -1,10 +1,52 @@
 export default function Spending() {
   return (
     <div class="spending-box">
-      <p>
-        Spending - Last 7 days mon tue wed thu fri sat sun Total this month
-        $478.33 +2.4% from last month
-      </p>
+      <h2>Spending - Last 7 days</h2>
+      <section class="graph-container">
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>mon</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>tue</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>wed</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>thu</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>fri</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>sat</h5>
+        </div>
+        <div class="graph-column">
+          <div class="graph-bar-container">
+            <div></div>
+          </div>
+          <h5>sun</h5>
+        </div>
+      </section>
+      <p>Total this month $478.33 +2.4% from last month</p>
     </div>
   );
 }
