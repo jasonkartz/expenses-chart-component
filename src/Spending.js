90f01847200b9f9f4210062e270069fb5ba91dd7
Jason Kartz <jasonkartz@gmail.com>
2022-06-27 22:25:52 -0500

    update

--- src/Spending.js
+++ src/Spending.js
@@ -5,12 +5,14 @@ export default function Spending() {
       <section class="graph-container">
         <div class="graph-column">
           <div class="graph-bar-container">
+            <div class="graph-bar-label">$56.32</div>
             <div class="dummy-bar" style={{ height: "75%" }}></div>
           </div>
           <h5>mon</h5>
         </div>
         <div class="graph-column">
           <div class="graph-bar-container">
+            <div class="graph-bar-label">$46.32</div>
             <div class="dummy-bar" style={{ height: "25%" }}></div>
           </div>
           <h5>tue</h5>
@@ -46,7 +48,16 @@ export default function Spending() {
           <h5>sun</h5>
         </div>
       </section>
-      <p>Total this month $478.33 +2.4% from last month</p>
+      <section class="total-section">
+        <div class="total-section-left">
+          <h5>Total this month</h5>
+          <h1>$478.33</h1>
+        </div>
+        <div class="total-section-right">
+          <h4>+2.4%</h4>
+          <h5>from last month</h5>
+        </div>
+      </section>
     </div>
   );
 }
